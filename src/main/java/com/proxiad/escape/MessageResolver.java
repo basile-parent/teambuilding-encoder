ecb62123eb476837b20f7602094420fb0b758e1a
Basile <basile.parent@arvato.fr>
2018-11-19 14:45:33 +0100

    Clues by the tests

--- src/main/java/com/proxiad/escape/MessageResolver.java
+++ src/main/java/com/proxiad/escape/MessageResolver.java
@@ -2,16 +2,4 @@ package com.proxiad.escape;
 
 public class MessageResolver {
 
-    public static void main(String[] args) {
-
-    }
-
-    public String decode(String message) {
-        return null;
-    }
-
-    public String decode(String message, String key) {
-        return null;
-    }
-
 }
--- src/test/java/com/proxiad/escape/MessageResolverTest.java
+++ src/test/java/com/proxiad/escape/MessageResolverTest.java
@@ -7,10 +7,14 @@ import static org.junit.Assert.*;
 public class MessageResolverTest {
 
     private MessageResolver messageResolver = new MessageResolver();
+    private static final String ALPHABET = "BESFCOYAJDLQZPRVXTMIWUHNGK";
 
     @Test
-    public void test_decode_shouldReturnDecodedString() {
-        assertNull(messageResolver.decode("message"));
+    public void test_decode_shouldReturnDecodedStringFromAlphabet() {
+        assertEquals("BBB", messageResolver.decode("AAA", ALPHABET, 1));
+        assertEquals("BESFC", messageResolver.decode("ABCDE", ALPHABET, 1));
+        assertEquals("ECMOS", messageResolver.decode("ABCDE", ALPHABET, 2));
+        assertEquals("ECMOS", messageResolver.decode("ABCDE", ALPHABET, 2));
     }
 
 }
