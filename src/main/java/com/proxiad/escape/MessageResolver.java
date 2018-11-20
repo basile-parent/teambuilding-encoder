011fcc948ad65ce613b345d6ff9c7e01f4460abd
Basile <basile.parent@arvato.fr>
2018-11-20 15:18:36 +0100

    Formulation de l'enigme

--- src/main/java/com/proxiad/escape/MessageResolver.java
+++ src/main/java/com/proxiad/escape/MessageResolver.java
@@ -2,4 +2,7 @@ package com.proxiad.escape;
 
 public class MessageResolver {
 
+    public String decode(String arg1, String arg2, int arg3) {
+        return null;
+    }
 }
--- src/test/java/com/proxiad/escape/MessageResolverTest.java
+++ src/test/java/com/proxiad/escape/MessageResolverTest.java
@@ -7,14 +7,17 @@ import static org.junit.Assert.*;
 public class MessageResolverTest {
 
     private MessageResolver messageResolver = new MessageResolver();
-    private static final String ALPHABET = "BESFCOYAJDLQZPRVXTMIWUHNGK";
 
     @Test
     public void test_decode_shouldReturnDecodedStringFromAlphabet() {
-        assertEquals("BBB", messageResolver.decode("AAA", ALPHABET, 1));
-        assertEquals("BESFC", messageResolver.decode("ABCDE", ALPHABET, 1));
-        assertEquals("ECMOS", messageResolver.decode("ABCDE", ALPHABET, 2));
-        assertEquals("ECMOS", messageResolver.decode("ABCDE", ALPHABET, 2));
+        final String arg2 = "BESFCOYAJDLQZPRVXTMIWUHNGK";
+
+        assertEquals("BBB", messageResolver.decode("AAA", arg2, 1));
+        assertEquals("BESFC", messageResolver.decode("ABCDE", arg2, 1));
+        assertEquals("ECMOS", messageResolver.decode("ABCDE", arg2, 2));
+        assertEquals("ECMOS", messageResolver.decode("ABCDE", arg2, 2));
     }
+
+    // Enigme à résoudre : TEIFLIFWEGOELEXE - DBKREQICJVLTZYNGWXMSOFHAPU - 11
 
 }
