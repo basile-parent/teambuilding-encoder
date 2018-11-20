c609589e40417d5b294e9b430ce907f0b790e0b3
Basile <basile.parent@arvato.fr>
2018-11-20 15:19:43 +0100

    Refacto

--- src/test/java/com/proxiad/escape/MessageResolverTest.java
+++ src/test/java/com/proxiad/escape/MessageResolverTest.java
@@ -7,20 +7,24 @@ import static org.junit.Assert.*;
 public class MessageResolverTest {
 
     private MessageResolver messageResolver = new MessageResolver();
-    private static final String ALPHABET = "BESFCOYAJDLQZPRVXTMIWUHNGK";
 
     @Test
     public void test_decode_shouldReturnDecodedStringFromAlphabet() {
+        final String ALPHABET = "BESFCOYAJDLQZPRVXTMIWUHNGK";
+
         assertEquals("BBB", messageResolver.decode("AAA", ALPHABET, 1));
         assertEquals("BESFC", messageResolver.decode("ABCDE", ALPHABET, 1));
         assertEquals("ECMOS", messageResolver.decode("ABCDE", ALPHABET, 2));
-        assertEquals("JESUISUNETHEIERE", messageResolver.decode("TEIFLIFWEGOELEXE", "DBKREQICJVLTZYNGWXMSOFHAPU", 11));
+        assertEquals("ECMOS", messageResolver.decode("ABCDE", ALPHABET, 2));
     }
 
     @Test
     public void test_encode_shouldReturnEncodedStringForEnigma() {
-        assertEquals("ABCDE", messageResolver.encode("BESFC", ALPHABET, 1));
-        assertEquals("TEIFLIFWEGOELEXE", messageResolver.encode("JESUISUNETHEIERE", "DBKREQICJVLTZYNGWXMSOFHAPU", 11));
+        final String ALPHABET_TEST = "BESFCOYAJDLQZPRVXTMIWUHNGK";
+        assertEquals("ABCDE", messageResolver.encode("BESFC", ALPHABET_TEST, 1));
+
+        final String ALPHABET_ENIGME = "DBKREQICJVLTZYNGWXMSOFHAPU";
+        assertEquals("TEIFLIFWEGOELEXE", messageResolver.encode("JESUISUNETHEIERE", ALPHABET_ENIGME, 11));
     }
     // Enigme à résoudre : TEIFLIFWEGOELEXE - DBKREQICJVLTZYNGWXMSOFHAPU - 11
 
