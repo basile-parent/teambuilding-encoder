262c130d89e360108e98d2ca86e60d580902b8b9
Basile <basile.parent@arvato.fr>
2018-12-12 14:44:01 +0100

    Refacto tests

--- src/test/java/com/proxiad/escape/MessageResolverTest.java
+++ src/test/java/com/proxiad/escape/MessageResolverTest.java
@@ -7,15 +7,25 @@ import static org.junit.Assert.*;
 public class MessageResolverTest {
 
     private MessageResolver messageResolver = new MessageResolver();
+    private static final String arg2 = "BESFCOYAJDLQZPRVXTMIWUHNGK";
 
     @Test
-    public void test_decode_shouldReturnDecodedStringFromAlphabet() {
-        final String ALPHABET = "BESFCOYAJDLQZPRVXTMIWUHNGK";
+    public void test_decode_shouldReturnDecodedStringFromAlphabetWithOneRepetition() {
+        assertEquals("BBB", messageResolver.decode("AAA", arg2, 1));
+        assertEquals("BESFC", messageResolver.decode("ABCDE", arg2, 1));
+        assertEquals("VTRNJBF", messageResolver.decode("PROXIAD", arg2, 1));
+    }
 
-        assertEquals("BBB", messageResolver.decode("AAA", ALPHABET, 1));
-        assertEquals("BESFC", messageResolver.decode("ABCDE", ALPHABET, 1));
-        assertEquals("ECMOS", messageResolver.decode("ABCDE", ALPHABET, 2));
-        assertEquals("ECMOS", messageResolver.decode("ABCDE", ALPHABET, 2));
+    @Test
+    public void test_decode_shouldReturnDecodedStringFromAlphabetWithTwoRepetitions() {
+        assertEquals("EEE", messageResolver.decode("AAA", arg2, 2));
+        assertEquals("ECMOS", messageResolver.decode("ABCDE", arg2, 2));
+        assertEquals("UITPDEO", messageResolver.decode("PROXIAD", arg2, 2));
+    }
+
+    @Test
+    public void test_decode_shouldReturnDecodedStringFromAlphabetWithThreeRepetitions() {
+        assertEquals("CCC", messageResolver.decode("AAA", arg2, 3));
     }
 
     @Test
